d714a49eb9d14ad923dc314ec0b773ebf3e1e9c3
ksunphd <kaysunphd@gmail.com>
2021-12-04 09:18:32 -0500

    cloud developer azure project 2

---
+++ cloud_developer_azure_nanodegree/project_2_deploy_neighborly_app_with_Azure_Functions/NeighborlyAPI/getAdvertisements/__init__.py
@@ -0,0 +1,23 @@
+import azure.functions as func
+import pymongo
+import json
+from bson.json_util import dumps
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+
+    try:
+        url = "******"
+        client = pymongo.MongoClient(url)
+        database = client['neighborhooddata']
+        collection = database['advertisements']
+
+
+        result = collection.find({})
+        result = dumps(result)
+
+        return func.HttpResponse(result, mimetype="application/json", charset='utf-8')
+    except:
+        print("could not connect to mongodb")
+        return func.HttpResponse("could not connect to mongodb",
+                                 status_code=400)
+
