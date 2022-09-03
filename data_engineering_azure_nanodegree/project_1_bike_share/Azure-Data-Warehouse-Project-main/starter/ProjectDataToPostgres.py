78ea3b373d2a5e9048ba49e778a063e1c4c5996c
ksunphd <kaysunphd@gmail.com>
2022-09-03 19:43:56 -0400

    remove leaks.

--- data_engineering_azure_nanodegree/project_1_bike_share/Azure-Data-Warehouse-Project-main/starter/ProjectDataToPostgres.py
+++ data_engineering_azure_nanodegree/project_1_bike_share/Azure-Data-Warehouse-Project-main/starter/ProjectDataToPostgres.py
@@ -4,15 +4,14 @@ from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 ########################################
 # Update connection string information #
 ########################################
-host = "warehouse1.postgres.database.azure.com"
-user = "udacityadmin"
-password = "qwerty123!"
+host = "****.postgres.database.azure.com"
+user = ""
+password = ""
 
 # Create a new DB
 sslmode = "require"
 dbname = "postgres"
 conn_string = "host={0} user={1} dbname={2} password={3} sslmode={4}".format(host, user, dbname, password, sslmode)
-# conn_string = "postgres://udacityadmin:qwerty123!@udacityproject1.postgres.database.azure.com/postgres?sslmode=require"
 conn = psycopg2.connect(conn_string)
 conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT);
 print("Connection established")
